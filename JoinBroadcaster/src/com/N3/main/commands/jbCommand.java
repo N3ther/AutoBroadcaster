3f35891d936c9e7fb8aebea749257a1832e70a9e
N3ther <thenetherreactor@gmail.com>
2019-06-02 13:07:36 -0400

    Add files via upload

--- JoinBroadcaster/src/com/N3/main/commands/jbCommand.java
+++ JoinBroadcaster/src/com/N3/main/commands/jbCommand.java
@@ -10,16 +10,36 @@ public class jbCommand extends JavaPlugin implements CommandExecutor {
 
 	// This method is called, when somebody uses our command
     @Override
-    public boolean onCommand(CommandSender sender, Command jbCommand, String jb, String[] reload) {
+    public boolean onCommand(CommandSender sender, Command jbCommand, String jb, String[] args) {
     	  if (sender instanceof Player) {
               Player player = (Player) sender;
               this.saveDefaultConfig();
               this.reloadConfig();
               this.getConfig();
               sender.sendMessage("Reload Complete");
+              if (args[-1].equalsIgnoreCase("jb")) {
+            	  if (args.length == 0) { //Sender only typed '/jb' and nothing else
+            	  sender.sendMessage("&9JoinBroadcaster Commands:");
+            	  sender.sendMessage("&6/jb: &9Brings up this menu");
+            	  sender.sendMessage("&6/jb reload: &9Reloads the config.yml");
+            	   
+            	  } else { //Sender typed more then 1 argument, so args[0] can't be null.
+            	  if (args[0].equalsIgnoreCase("reload")) { //Sender typed '/jb reload'
+            	  sender.sendMessage("&9Starting reload...");
+            	  this.saveDefaultConfig();
+            	  this.reloadConfig();
+            	  this.getConfig();
+            	  sender.sendMessage("&9Reload complete!");
+            	  } else { //Sender had attest 1 argument, but didn't type sir as a second one!
+            	  //args[0] also returns string, so lets send the message right back to them!
+            	  sender.sendMessage("Your first argument was: " + args[0]);
+            	   
+            	  }
+            	  }
           }
 
           // If the player (or console) uses our command correct, we can return true
           return true;
     }
+}
 }
